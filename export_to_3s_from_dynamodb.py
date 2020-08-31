d32a621df805eb50e144ae5a298c70d4acb33893
Seunghun Geum <shgeum81@gmail.com>
2020-09-01 02:46:54 +0900

    Update export_to_3s_from_dynamodb.py

--- export_to_3s_from_dynamodb.py
+++ export_to_3s_from_dynamodb.py
@@ -1,8 +1,11 @@
 import sys
 import datetime
+from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -15,6 +18,10 @@ table = glueContext.create_dynamic_frame.from_options(
     "dynamodb.splits": "100"
   }
 )
+
+table = table.apply_mapping([
+('원본필드명','원본데이터형','수정필드명','수정데이터형')
+])
 
 glueContext.write_dynamic_frame.from_options(
   frame=table,
